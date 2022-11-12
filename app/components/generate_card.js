4f1e447e55efb044c6d6eb8f7da99dd387ec6a8a
vitorgabrielgit <vitorgabrieldeoliveiragit@gmail.com>
2022-11-12 19:42:34 -0300

    BUG sistema de filtro de item, nn está imprimindo os cards dentro do container

--- app/components/generate_card.js
+++ app/components/generate_card.js
@@ -13,10 +13,36 @@ const titles = [
     'Pasta Térmica',
     'HD',
     'SSD',
+    'Placa Mãe',
+    'Mouse',
+    'Teclado',
+    'Monitor',
+    'Mouse pad',
+    'Gabinete',
+    'Processador',
+    'Fonte',
+    'Place de Video',
+    'Cooler',
+    'Pasta Térmica',
+    'HD',
+    'SSD',
 ]; 
 let token_title = 0;
 
 const description = [
+    'Placa mãe de qualidade',
+    'Mouse 4000 dpi',
+    'Teclado semi-mecanico',
+    'Monitor 144hz',
+    'Suave e grande',
+    'Modelo Slim',
+    'i9 - 8 Núcleo',
+    '350w',
+    '3020TI',
+    '5000 RPM',
+    'Ótima qualidade',
+    '1TB',
+    '140GB',
     'Placa mãe de qualidade',
     'Mouse 4000 dpi',
     'Teclado semi-mecanico',
@@ -47,6 +73,19 @@ const prices = [
     '25.00',
     '85.50',
     '135.00',
+    '650.00',
+    '130.00',
+    '350.00',
+    '1200.00',
+    '60.00',
+    '230.00',
+    '5.200',
+    '450.00',
+    '3.500',
+    '35.00',
+    '25.00',
+    '85.50',
+    '135.00',
 ];
 let token_prices = 0;
 // ========== * Generate Card * ==========
@@ -60,7 +99,6 @@ function loop_card() {
 };
 
 function controls() {
-    let container = document.querySelector('.container_card')
     $(".container_card").append(`
         <div class="card">
             <div class="card-img"></div>
@@ -82,48 +120,51 @@ function controls() {
 };
 
 function search() {
+    let token_remove_items = 0;
+    var array_item_print = [];
     let search = document.querySelector('#search_name').value;
     if(search) {
         titles.forEach((element, index) => {
             if(element.toLocaleLowerCase() === search.toLocaleLowerCase()) {
-                $(".container_card").remove("div");
-
-                let i = index;
-
-                var object = {
-                    token_title: i,
-                    token_description: i,
-                    token_prices: i,
-                    token_search: 1,
-                };
-
-               
-
+                token_remove_items++;
+                array_item_print.push(index);
             };
         });
+        if(token_remove_items >= 1) {
+            $(".container_card").remove("div");
 
-         search_print(object);
+            array_item_print.forEach(element => {
+                var object = {
+                    token_title: titles[element],
+                    token_prices: prices[element],
+                    token_description: description[element],
+                };
+                print_renove(object);
+            });
+            token_remove_items = 0;
+        } else {
+            $(".container_card").remove("div");
+        };
     };
 };
 
-function search_print(object) {
-    if(object.token_search == 1) {
-        $(".container_card").append(`
+function print_renove(object) {
+    console.log(object);
+    $(".container_card").append(`
         <div class="card">
             <div class="card-img"></div>
             <div class="card-info">
-                <p class="text-title">${titles[object.token_title]}</p>
-                <p class="text-body">${description[object.token_description]}</p>
+                <p class="text-title">${object.token_title}</p>
+                <p class="text-body">${object.token_description}</p>
             </div>
             <div class="card-footer">
-                <span class="text-title">${prices[object.token_prices]}</span>
+                <span class="text-title">${object.token_prices}</span>
                 <div onclick="buy(${object.token_title})" class="card-button">
                     <i class="svg_icon bi bi-cart-fill"></i>
                 </div>
             </div>
         </div>
-        `);
-    };
+    `);
 };
 
 function buy(item) {
